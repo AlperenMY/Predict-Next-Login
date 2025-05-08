131fabd523268cbb71fcb6a7ed248af65075ad2a
Alperen M. YILMAZ <alperenm.yilmaz@gmail.com>
2025-05-08 17:03:20 +0300

    added render frontend url to allowedHost

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	preview: {
+		port: 4173, // Render yine $PORT üzerinden override edecek
+		host: true, // 0.0.0.0 üzerinden dışarı açılır
+		allowedHosts: ["predict-next-login-1.onrender.com"],
+	},
+});
