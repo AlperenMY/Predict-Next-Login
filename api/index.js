9104fc7928d7e64a4ba919e37439469d171de436
Alperen M. YILMAZ <alperenm.yilmaz@gmail.com>
2025-05-08 16:37:11 +0300

    update port for deployment purposes

--- api/index.js
+++ api/index.js
@@ -202,6 +202,7 @@ app.get("/predictLogins", async (req, res) => {
 	}
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
 	console.log(`LISTENING ON PORT 3000`);
 });
